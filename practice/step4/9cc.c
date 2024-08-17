9ae6e3771d943ccd2832c06ffec8dc79858a72e1
mana2216 <ms00lx.221b@gmail.com>
2024-08-17 18:03:50 +0900

    ステップ5の練習

--- practice/step4/9cc.c
+++ practice/step4/9cc.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include <stdlib.h>
 
 typedef enum {
   TK_RESERVED,
   TK_NUM,
   TK_EOF,
 } TokenKind;
+
+char *user_input;
 
 typedef struct Token Token;
 
@@ -30,8 +32,6 @@ void error(char *fmt, ...) {
   exit(1);
 }
 
-char *user_input;
-
 void error_at(char *loc, char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -46,7 +46,7 @@ void error_at(char *loc, char *fmt, ...) {
 }
 
 bool consume(char op) {
-  if (TK_RESERVED != token->kind || op != token->str[0]) {
+  if (token->kind != TK_RESERVED || token->str[0] != op) {
     return false;
   }
   token = token->next;
@@ -55,7 +55,7 @@ bool consume(char op) {
 
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op) {
-    error_at(token->str, "expected %c", op);
+    error_at(token->str, "'%c'ではありません", op);
   }
   token = token->next;
 }
@@ -93,7 +93,7 @@ Token *tokenize(void) {
       continue;
     }
 
-    if (strchr("+-*/()", *p)) {
+    if (*p == '+' || *p == '-') {
       cur = new_token(TK_RESERVED, cur, p);
       p++;
       continue;
@@ -104,118 +104,14 @@ Token *tokenize(void) {
       cur->val = strtol(p, &p, 10);
       continue;
     }
-
+    
     error_at(p, "トークナイズできません");
   }
-
   new_token(TK_EOF, cur, p);
   return head.next;
 }
 
-typedef enum {
-  ND_ADD,
-  ND_SUB,
-  ND_MUL,
-  ND_DIV,
-  ND_NUM,
-} NodeKind;
 
-typedef struct Node Node;
-
-struct Node {
-  NodeKind kind;
-  Node *lhs;
-  Node *rhs;
-  int val;
-};
-
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return node;
-}
-
-Node *new_node_num(int val) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = ND_NUM;
-  node->val = val;
-  return node;
-}
-
-Node *expr(void);
-Node *mul(void);
-Node *primary(void);
-
-Node *expr(void) {
-  Node *node = mul();
-
-  for (;;) {
-    if (consume('+')) {
-      node = new_node(ND_ADD, node, mul());
-    } else if (consume('-')) {
-      node = new_node(ND_SUB, node, mul());
-    } else {
-      return node;
-    }
-  }
-}
-
-Node *mul(void) {
-  Node *node = primary();
-
-  for (;;) {
-    if (consume('*')) {
-      node = new_node(ND_MUL, node, primary());
-    } else if (consume('/')) {
-      node = new_node(ND_DIV, node , primary());
-    } else {
-      return node;
-    }
-  }
-}
-
-Node *primary(void) {
-  if (consume('(')) {
-    Node *node = expr();
-    consume(')');
-    return node;
-  }
-
-  return new_node_num(expect_number());
-}
-
-void gen(Node *node) {
-  if (node->kind == ND_NUM) {
-    printf("  push %d\n", node->val);
-    return;
-  }
-
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->kind) {
-    case ND_ADD:
-      printf("  add rax, rdi\n");
-      break;
-    case ND_SUB:
-      printf("  sub rax, rdi\n");
-      break;
-    case ND_MUL:
-      printf("  imul rax, rdi\n");
-      break;
-    case ND_DIV:
-      printf("  cqo\n");
-      printf("  idiv rdi\n");
-      break;
-  }
-
-  printf("  push rax\n");
-}
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -225,15 +121,23 @@ int main(int argc, char *argv[]) {
 
   user_input = argv[1];
   token = tokenize();
-  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  gen(node);
+  printf("  mov rax, %d\n", expect_number());
 
-  printf("  pop rax\n");
+  while (!at_eof()) {
+    if (consume('+')) {
+      printf("  add rax, %d\n", expect_number());
+      continue;
+    }
+
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
+  }
+
   printf("  ret\n");
   return 0;
 }
